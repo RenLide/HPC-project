d26733b28cb91da90ca2d8070bd52ca0dc7a29da
renlide <1308139298@qq.com>
2022-06-08 23:53:39 +0800

    update explicit.c

--- explicit.c
+++ explicit.c
@@ -1,310 +1,163 @@
-// Finite-difference method to slove 2D heat transfer problem
-// Square Omega of dimension 1 by 1
-// Explicit Euler scheme for the time discretization
-// Finite difference method for the spatial discretization
-// Use HDF5 to store output data
-//
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <petscksp.h>
-#include <petsc.h>
-#include <petscvec.h>
-#include "petscmat.h"
-#include "global.h"
-#include "hdf5.h"
-#define FILE2 "heat_trans_data.h5"
+static char help[] = "Solves a tridiagonal linear system.\n\n";
+#include<petscksp.h>
+#include<petscmath.h>
+#include<petscviewerhdf5.h>
+#include<math.h>
 
-static char help[] = "Explicit EULER method\n\n";
+#define PI      3.1415926
+#define GRID    100
+#define RHO     1.0
+#define K       1.0
+#define C       1.0
+#define XEND    1.0
+#define TSTEP   100
+#define TEND    2.0
+int main(int argc, char **args){
+    
+    
+/* ******************定义变量*********************** */
+    Vec x, uj, uj_old;
+    Mat A;
+    PetscErrorCode ierr;
+    PetscInt rank, START = 0, END=XEND, pos, x_start, x_end;
+    PetscBool r = PETSC_FALSE;
+    PetscReal CFL, dx, dt, u0, f, t = 0, t_start=0, t_end=TEND;
+    PetScalar col[3], coef[3], info[3], zero = 0.0;
+    PetViewer h5;
+/* *********************************************** */
+    
+    
+/* ******************初始化参数********************* */
+    ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;    /*初始化Petsc*/
+    ierr = PetscOptionsBegin(PETSC_COMM_WORLD,NULL,NULL,NULL);CHKERRQ(ierr);    /*开始读取选项参数*/
+    ierr = PetscOptionsGetBool(NULL,NULL,"-r",&r,NULL);CHKERRQ(ierr);    /*从命令行读取是否重启（若有）*/
+    ierr = PetscOptionsEnd();CHKERRQ(ierr);    /*读取选项参数结束*/
+    ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank);CHKERRQ(ierr);    /*设置并行MPI参数*/
+/* *********************************************** */
+    
 
-int main(int argc, char** args) {
-	// allocate two dimensional array (N+1)*(N+1)
-	double** u = (double**)malloc(sizeof(float*) * (N + 2));
-	for (int i = 0; i < (N + 2); i++) {
-		u[i] = (double*)malloc(sizeof(double) * (N + 2));
-	}
-	double** unew = (double**)malloc(sizeof(float*) * (N + 2));
-	for (int i = 0; i < (N + 2); i++) {
-		unew[i] = (double*)malloc(sizeof(double) * (N + 2));
-	} 
-	double** f = (double**)malloc(sizeof(float*) * (N));
-	for (int i = 0; i < (N); i++) {
-		f[i] = (double*)malloc(sizeof(double) * (N));
-	}
-	double x, y;
-	double dx;
-	dx = (double)L / (double)N;
-	double dy;
-	dy = (double)L / (double)N;
-	FILE* init;
-	FILE* uout;
-	double pi = 4.0 * atan(1.0);
-	printf("%d, %f, %f, %f\n", N, dx, dy, dt);
+/* *************计算并打印初始化变量信息*************** */
+    dx = 1.0/n;    /*计算dx&dt*/
+    dt = (t_end - t_start)/TSTEP;
+    CFL= K*dt/(RHO*C*dx*dx);    /*计算CFL值*/
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"dx = %f\t",dx);CHKERRQ(ierr);    /*将dx的值打印出来，方便阅读输出文件时参考*/
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"dt = %f\t",dt);CHKERRQ(ierr);
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"CFL = %f\n",CFL);CHKERRQ(ierr);
+    ierr = PetscPrintf(PETSC_COMM_WORLD, "grid = %d tstep = %d\n", GRID, TSTEP);CHKERRQ(ierr);
+    ierr = PetscPrintf(PETSC_COMM_WORLD,"restart is %d\n",restart);CHKERRQ(ierr);
+/* *********************************************** */
+    
+    
+/* ******************初始化参数********************* */
+    ierr = VecCreate(PETSC_COMM_WORLD,&x);CHKERRQ(ierr);    /*创建一个并行空间*/
+    ierr = VecCreate(PETSC_COMM_WORLD,&info);CHKERRQ(ierr);    /*创建临时向量*/
+    ierr = VecSetSizes(x,PETSC_DECIDE,n+1);CHKERRQ(ierr);    /*创建一个长度n+1的矩阵*/
+    ierr = VecSetSizes(info, 3, PETSC_DECIDE);CHKERRQ(ierr);    /*创建长度为3的临时向量*/
+    ierr = VecSetFromOptions(x);CHKERRQ(ierr);    /*从选项数据库中配置向量*/
+    ierr = VecSetFromOptions(info);CHKERRQ(ierr);    /*获得参数*/
+    ierr = VecDuplicate(uj,&uj);CHKERRQ(ierr);    /*将x的格式赋给z*/
+    ierr = VecDuplicate(uj,&uj_old);CHKERRQ(ierr);    /*将x的格式赋给b*/
 
-	double forward(double ujk, double ujmk, double ujpk, double ujkm, double ujkp, double x, double y);
+    ierr = VecGetOwnershipRange(x,&START,&END);CHKERRQ(ierr);    /*设置并行x的起始终止点*/
+    ierr = VecGetLocalSize(x,&pos);CHKERRQ(ierr);    /*设置并行x的位置点*/
 
-	u[0][0] = 0.0;
-	unew[0][0] = 0.0;
-	u[N + 1][N + 1] = 0.0;
-	unew[N + 1][N + 1] = 0.0;
-	u[0][N + 1] = 0.0;
-	unew[0][N + 1] = 0.0;
-	u[N + 1][0] = 0.0;
-	unew[N + 1][0] = 0.0;
-	// grid init (x-d first, then y-d)
-	// use initial condition
-	for (int j = 1; j < (N + 1); j++) {
-		y = (j - 0.5) * dy;
-		for (int i = 1; i < (N + 1); i++) {
-			x = (i - 0.5) * dx;
-			u[i][j] = exp(x + y);
-		}
-	}
-	// use boundary conditions
-	// just set top & bottom as tau_g, left & right as tau_h
-	for (int i = 1; i < (N + 1); i++) {
-		u[i][0] = g;
-		u[i][N + 1] = g;
-		u[N + 1][i] = g;
-		u[0][i] = g;
+    ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);    /*在并行空间创建一个矩阵*/
+    ierr = MatSetSizes(A,pos,pos,n+1,n+1);CHKERRQ(ierr);    /*设置矩阵的行数和列数*/
+    ierr = MatSetFromOptions(A);CHKERRQ(ierr);    /*从选项数据库中配置矩阵*/
+    ierr = MatSetUp(A);CHKERRQ(ierr);    /*开始建立矩阵*/
+/* *********************************************** */
 
-	}
-	// for (int i = 1; i < (N + 1); i++) {
-	//	 u[N + 1][i] = u[N][i] + dx * h1 / kcond;
-	//	 u[0][i] = u[1][i] + dx * h2 / kcond;
-	// }
-	
-	// test code
-	init = fopen("init.csv", "w+");
-	for (int j = 0; j < (N + 2); j++) {
-		y = (j - 0.5) * dy;
-		for (int i = 0; i < (N + 2); i++) {
-			x = (i - 0.5) * dx;
-			fprintf(init, "%f,%f,%f\n", x, y, u[i][j]);
-		}
-	}
-	fclose(init);
+    
+/* ****************初始化三对角矩阵****************** */
+    coef[0] = CFL; coef[1] = 1 -2 * CFL; coef[2] = CFL;
+    for(i = START + 1; i < END; i++){
+        col[0] = i - 1; col[1] = i; col[2] = i + 1;
+        ierr = MatSetValues(A,1,&i,3,col,coef,INSERT_VALUES);CHKERRQ(ierr);
+    }
+    
+    col[0] = 0; col[1] = 1;
+    coef[0] = 1 - 2 * CFL; coef[1] = CFL;
+    ierr = MatSetValues(A,1,&i,2,col,coef,INSERT_VALUES);CHKERRQ(ierr);
+    
+    col[0] = END - 1; col[1] = END;
+    coef[0] = 1 - 2 * CFL; coef[1] = CFL;
+    ierr = MatSetValues(A,1,&i,2,col,coef,INSERT_VALUES);CHKERRQ(ierr);
 
+    ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);    /*通知其余并行块将矩阵统一*/
+    ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);    /*结束通知*/
+    ierr = MatView(A, PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);    /*打印矩阵，检查是否出错*/
+/* *********************************************** */
+    
+    
+/* ***********根据-r参数对uj_old数据初始化************** */
+    if(r){
+        ierr = VecSet(uj_old, 0);CHKERRQ(ierr);    /*设置初始向量z*/
+        if(rank == 0){    /*开始设置初始条件*/
+          for(int i = 1; i < END; i++){    /*除首尾两个点外的其余点*/
+            u0 = exp(i*dx);    /*根据当前位置来获取初始值*/
+              ierr = VecSetValues(uj_old, 1, &i, &u0, INSERT_VALUES);CHKERRQ(ierr);    /*将向量的对应位置的值进行修改*/
+          }
+        }
+        ierr = VecAssemblyBegin(uj_old);CHKERRQ(ierr);    /*通知其余并行块将向量统一*/
+        ierr = VecAssemblyEnd(uj_old);CHKERRQ(ierr);    /*结束通知*/
+    }
+    else{
+        ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD,"explicit.h5", FILE_MODE_READ, &h5);CHKERRQ(ierr);    /*创建输入文件*/
+        ierr = PetscObjectSetName((PetscObject) uj_old, "uj");CHKERRQ(ierr);    /*将z输入的名字命名为explicit-vector*/
+        ierr = PetscObjectSetName((PetscObject) info, "info");CHKERRQ(ierr);    /*将临时向量tem输入的名字命名为explicit-necess-data*/
+        ierr = VecLoad(info, h5);CHKERRQ(ierr);    /*将读入的数据加载到向量tem中*/
+        ierr = VecLoad(uj_old, h5);CHKERRQ(ierr);    /*将读入的数据加载到向量z中*/
+        ierr = PetscViewerDestroy(&h5);CHKERRQ(ierr);    /*关闭输入*/
+        index=0;    /*将索引初始化*/
+        ierr = VecGetValues(info,1,&index,&dx);CHKERRQ(ierr);    /*将第一个值赋给dx*/
+        index=index+1;    /*索引移向下一位*/
+        ierr = VecGetValues(info,1,&index,&dt);CHKERRQ(ierr);    /*将第二个值赋给dt*/
+        index=index+1;    /*索引移向下一位*/
+        ierr = VecGetValues(info,1,&index,&t);CHKERRQ(ierr);    /*将第三个值赋给t*/
+        index= 0;    /*索引复位*/
+    }
+    ierr = VecSet(b,zero);CHKERRQ(ierr);    /*设置初始向量b*/
+    if(rank == 0){    /*开始设置初始条件*/
+      for(int i = 1; i < END; i++){    /*除首尾两个点外的其余点*/
+        f = dt*sin(i*dx*PI);    /*根据当前位置来获取传热值*/
+        ierr = VecSetValues(b, 1, &i, &f, INSERT_VALUES);CHKERRQ(ierr);    /*将向量的对应位置的值进行修改*/
+      }
+    }
+    ierr = VecAssemblyBegin(f);CHKERRQ(ierr);    /*通知其余并行块将向量统一*/
+    ierr = VecAssemblyEnd(f);CHKERRQ(ierr);    /*结束通知*/
+/* *********************************************** */
+    
+    
+/* ****************迭代计算uj值********************* */
+    while(t >= 0 && t <= 2.0){    /*计算0-2时间内的传播*/
+       t += dt;    /*时间向前走*/
+       ierr = MatMult(A,uj_old,uj);CHKERRQ(ierr);    /*求得下一个时刻的温度热量值*/
+       ierr = VecAXPY(uj,1.0,f);CHKERRQ(ierr);    /*将求得的值加上在时间步长内的*/
+       ierr = VecSetValues(uj, 1, &START, &zero, INSERT_VALUES);CHKERRQ(ierr);    /*设置边界条件*/
+       ierr = VecSetValues(uj, 1, &END, &zero, INSERT_VALUES);CHKERRQ(ierr);    /*设置边界条件*/
+       ierr = VecAssemblyBegin(uj);CHKERRQ(ierr);    /*统一向量更新*/
+       ierr = VecAssemblyEnd(uj);CHKERRQ(ierr);    /*结束更新*/
 
-	// Petsc Part
-	MPI_Comm       comm;
-	PetscMPIInt    rank;
+       iter += 1;    /*记录迭代次数*/
+       if((iter % 10) == 0){    /*如果迭代次数为10的倍数，即每迭代十次*/
+       
+        info[0] = dx; info[1] = dt; info[2] = t;    /*将值赋给数组*/
+        ierr = VecSet(info, 0.0);CHKERRQ(ierr);    /*初始化矩阵*/
+        for(index=0;index<3;index++){    /*循环遍历数组，并将值赋给向量*/
+          u0 = data[index];    /*将数组的值赋给u0*/
+          ierr = VecSetValues(tem,1,&index,&u0,INSERT_VALUES);CHKERRQ(ierr);    /*将矩阵赋值给向量*/
+        }
+        ierr = VecAssemblyBegin(tem);CHKERRQ(ierr);    /*通知其余并行块将向量统一*/
+        ierr = VecAssemblyEnd(tem);CHKERRQ(ierr);    /*结束通知*/
 
-	Mat            A, U, Uold, F;                /* linear system matrix */
-
-	PetscReal      err, fvalue, norm_uold = 0.0, norm_u = 1.0;  /* norm of solution error */
-	PetscErrorCode ierr;
-	PetscInt       cc, col[3], maxit = 5000, rstart, rend, rlocal, clocal;
-	PetscInt       max = 50000;
-	PetscInt       n = N;
-	PetscScalar    a = 1.0, value[3];
-
-	ierr = PetscInitialize(&argc, &args, (char*)0, help); if (ierr) return ierr;
-
-	// MPI initial
-	comm = MPI_COMM_WORLD;
-	MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
-
-	ierr = PetscOptionsGetInt(NULL, NULL, "-n", &n, NULL); CHKERRQ(ierr);
-	ierr = PetscOptionsGetReal(NULL, NULL, "-dt", &dt, NULL);
-
-	/*
-	Create matrix U.  When using MatCreate(), the matrix format can
-	be specified at runtime.
-	Performance tuning note:  For problems of substantial size,
-	preallocation of matrix memory is crucial for attaining good
-	performance. See the matrix chapter of the users manual for details.
-	We pass in nlocal as the "local" size of the matrix to force it
-	to have the same parallel layout as the vector created above.
-	*/
-	ierr = MatCreate(PETSC_COMM_WORLD, &U); CHKERRQ(ierr);
-	ierr = MatSetSizes(U, PETSC_DECIDE, PETSC_DECIDE, n, n); CHKERRQ(ierr);
-	ierr = MatSetFromOptions(U); CHKERRQ(ierr);
-	ierr = MatSetUp(U); CHKERRQ(ierr);
-	/* Identify the starting and ending mesh points on each
-	processor for the interior part of the mesh. We let PETSc decide
-	above. */
-	ierr = MatGetOwnershipRange(U, &rstart, &rend); CHKERRQ(ierr);
-	ierr = MatGetLocalSize(U, &rlocal, &clocal); CHKERRQ(ierr);
-
-	for (int j = 1; j < (N + 1); j++) {
-		for (int i = 1; i < (N + 1); i++) {
-			ierr = MatSetValue(U, i, j, u[i][j], INSERT_VALUES); CHKERRQ(ierr);
-		}
-	}
-	/* Assemble the matrix */
-	ierr = MatAssemblyBegin(U, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
-	ierr = MatAssemblyEnd(U, MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
-	ierr = MatView(U, PETSC_VIEWER_STDOUT_WORLD); CHKERRQ(ierr);
-
-	// Create coefs matrix A.
-	ierr = MatCreate(PETSC_COMM_WORLD, &A); CHKERRQ(ierr);
-	ierr = MatSetSizes(A, rlocal, clocal, n, n); CHKERRQ(ierr);
-	ierr = MatSetFromOptions(A); CHKERRQ(ierr);
-	ierr = MatMPIAIJSetPreallocation(A, 3, NULL, 2, NULL);
-	ierr = MatSetUp(A); CHKERRQ(ierr);
-	// Set values to A
-	MatGetOwnershipRange(A, &rstart, &rend);
-	double CFL = dt * kcond / dx / dx / rho / c + dt * kcond / dy / dy / rho / c;
-	int ii;
-	if (!rstart)
-	{
-		rstart = 1;
-		ii = 0; col[0] = 0; col[1] = 1; value[0] = 1.0 - 2.0 * CFL; value[1] = CFL;
-		ierr = MatSetValues(A, 1, &ii, 2, col, value, INSERT_VALUES); CHKERRQ(ierr);
-	}
-	if (rend == n)
-	{
-		rend = n - 1;
-		ii = n - 1; col[0] = n - 2; col[1] = n - 1; value[0] = CFL; value[1] = 1.0 - 2.0 * CFL;
-		ierr = MatSetValues(A, 1, &ii, 2, col, value, INSERT_VALUES); CHKERRQ(ierr);
-	}	
-	value[0] = CFL;
-	value[1] = 1.0 - 2.0 * CFL;
-	value[2] = CFL;
-	for (int i = rstart; i < rend; i++) {
-		col[0] = i - 1; col[1] = i; col[2] = i + 1;
-		MatSetValues(A, 1, &i, 3, col, value, INSERT_VALUES);
-	}
-	/* Assemble the matrix */
-	MatAssemblyBegin(A, MAT_FINAL_ASSEMBLY);
-	MatAssemblyEnd(A, MAT_FINAL_ASSEMBLY);
-	MatView(A, PETSC_VIEWER_STDOUT_WORLD);
-
-	/* Duplicate the pattern of u to other vectors */
-	MatDuplicate(U, &Uold);
-	MatDuplicate(U, &F);
-	
-	/* Set value for heat supply, F */
-	for (int j = 1; j < (N + 1); j++) {
-		y = (j - 0.5) * dy;
-		for (int i = 1; i < (N + 1); i++) {
-			x = (i - 0.5) * dx;
-			f[i][j] = sin(L * pi * (x + y)) * dt / rho / c;
-		}
-	}
-	MatGetOwnershipRange(F, &rstart, &rend);
-	for (int j = 1; j < (N + 1); j++) {
-		for (int i = 1; i < (N + 1); i++) {
-			ierr = MatSetValue(F, i, j, f[i][j], INSERT_VALUES); CHKERRQ(ierr);
-		}
-	}
-	MatAssemblyBegin(F);
-	MatAssemblyEnd(F);
-	MatView(F, PETSC_VIEWER_STDOUT_WORLD);
-
-	// calculate next step
-	// forward euler scheme
-	for (int iter = 0; iter < maxit; iter++) {
-		MatCopy(U, Uold);
-		MatMatMult(A, Uold, MAT_REUSE_MATRIX, fill, U);
-		MatAXPY(U, a, U, SAME_NONZERO_PATTERN);
-		MatNorm(U, NORM_1, &norm_u);
-		err = fabs(norm_u - norm_uold);
-		if (err < 1.e-8) {
-			PetscPrintf(comm, "End at %Dth iter, err=%g\n", iter + 1, err);
-			break;
-		}
-		norm_uold = norm_u;
-	}
-	MatView(U, PETSC_VIEWER_STDOUT_WORLD);
-
-
-	// output
-	// change as your needs (here, I print out t~=0.01)
-	// demo ver with csv file, should change with HDF5!!!
-	// still have problems here, should u get higher constantly in this BCs ?!!!
-	//	if (iter == (int)(0.01 / dt)) {
-	//		uout = fopen("uout.csv", "w+");
-	//		for (int j = 0; j < (N + 2); j++) {
-	//			y = (j - 0.5) * dy;
-	//			for (int i = 0; i < (N + 2); i++) {
-	//				x = (i - 0.5) * dx;
-	//				fprintf(uout, "%f,%f,%f\n", x, y, u[i][j]);
-	//			}
-	//		}
-	//		fclose(uout);
-	//	}
-
-	//	iter = iter + 1;
-	//}
-	for (int i = 0; i < (N + 2); i++) {
-		free(u[i]);
-		free(unew[i]);
-	}
-	for (int i = 0; i < (N); i++) {
-		free(f[i]);
-	}
-	free(u);
-	free(unew);
-	free(f);
-	MatDestroy(&A);
-	MatDestroy(&U);
-	MatDestroy(&F);
-	MatDestroy(&Uold);
-	PetscFinalize();
-
-	/* HDF5 initialization */
-	hid_t        file_id, dataset_id, group_id, dataspace_id;  /* identifiers */
-	hsize_t      dims[2];
-	herr_t       status;
-	int* vec1 = (int*)malloc((N + 2) * (N + 2) * sizeof(int));
-	free(vec1);
-
-	/* HDF5: Create a new file to store velocity datasets. */
-	file_id = H5Fcreate("heat_trans_data.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-
-	dims[0] = N + 2;
-	dims[1] = N + 2;
-
-	dataspace_id = H5Screate_simple(2, dims, NULL);
-
-	/* Create two groups to store initial values and output values in the file. */
-	group_id = H5Gcreate2(file_id, "/output", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-
-	/* Create the datasets. */
-	dataset_id = H5Dcreate2(file_id, "/output/uout", H5T_STD_I32BE, dataspace_id,
-		H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-
-
-	//  printf("original dset_data[0][0][0]:%2d\n", dset_data[0][0][0]);
-
-	 /* Write the first dataset. */
-	status = H5Dwrite(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT,
-		u);
-
-	/* Close the data space for the first dataset. */
-	status = H5Sclose(dataspace_id);
-
-	/* Close the first dataset. */
-	status = H5Dclose(dataset_id);
-	/* Close the group. */
-	status = H5Gclose(group_id);
-	status = H5Fclose(file_id);
-
-	return 0;
+        ierr = PetscViewerCreate(PETSC_COMM_WORLD,&h5);CHKERRQ(ierr);    /*创建输出指针*/
+        ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD,"explicit.h5", FILE_MODE_WRITE, &h5);CHKERRQ(ierr);    /*创建输出文件*/
+        ierr = PetscObjectSetName((PetscObject) z, "explicit-vector");CHKERRQ(ierr);    /*将z输出的名字命名为explicit-vector*/
+        ierr = PetscObjectSetName((PetscObject) tem, "explicit-necess-data");CHKERRQ(ierr);    /*将tem输出的名字命名为explicit-necess-data*/
+        ierr = VecView(tem, h5);CHKERRQ(ierr);    /*tem输出到文件*/
+        ierr = VecView(z, h5);CHKERRQ(ierr);    /*z输出到文件*/
+        ierr = PetscViewerDestroy(&h5);CHKERRQ(ierr);    /*关闭输出*/
+       }
+    }
+/* *********************************************** */
 }
-
-
-// eulersch function
-//double forward(double ujk, double ujmk, double ujpk, double ujkm, double ujkp, double x, double y) {
-//	double u1, u2;
-//	double ujknew;
-//	double dt;
-//	double f;
-//
-//	dt = Tend / (double)nsteps;
-//	double dx;
-//	dx = (double)L / (double)N;
-//	double dy;
-//	dy = (double)L / (double)N;
-//	double pi = 4.0 * atan(1.0);
-//
-//	double CFL1 = kcond * dt / (dx * dx);
-//	double CFL2 = kcond * dt / (dy * dy);
-//	f = sin(L * pi * (x + y));
-//	u1 = CFL1 * (ujpk - 2 * ujk + ujmk);
-//	u2 = CFL2 * (ujkp - 2 * ujk + ujkm);
-//	ujknew = ujk + 1 / (rho * c) * (f + u1 + u2);
-//	return ujknew;
-//}
